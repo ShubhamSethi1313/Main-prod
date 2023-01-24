f97eea63c96cf9cce19f4786b057140f81990b87
ShubhamSethi1313 <shubham@uxdlabtech.com>
2023-01-25 02:18:05 +0530

    main-prod

---
+++ app.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require('path')
+
+app.use(cookieParser());
+
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
+// const User = require('./model/userSchema')
+
+app.use(express.json());
+
+// link the router files to make our route easy
+app.use(require("./router/auth"));
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`server is running at port no ${PORT}`);
+});
